378f170bf25241b3312f61cafca48fb35b835cb3
cdenzelcoleman <camdcole@gmail.com>
2025-04-09 21:47:01 -0500

    add mobile contact info

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import "../public/pdf/Cameron_Coleman_Full_Stack_Resume.pdf";
+import "../public/pdf/CameronColemanFullStackResume.pdf";
 import "./css/contact.css";
 
 const Contact = () => {
