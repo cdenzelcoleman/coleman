610f4e1ce12a7488c68a32056416168a9e51b35b
cdenzelcoleman <camdcole@gmail.com>
2025-04-11 12:47:43 -0500

    Cursor

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,16 @@
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
-import App from "./App";
+import { BrowserRouter } from "react-router-dom"; 
 import "./css/index.css";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import SmoothScroll from "./utils/SmoothScroll";
 
 createRoot(document.getElementById("root")).render(
-  <HashRouter>
+  <BrowserRouter basename="/coleman">
     <SmoothScroll>
       <App />
       <Analytics />
       <SpeedInsights />
     </SmoothScroll>
-  </HashRouter>
+    </BrowserRouter>
 );
